2669e4724c90d4bb246ea4c6cede408290a2859d
Haroon Khan <hkhan@healthexpense.com>
2016-02-03 21:31:57 -0800

    Remove extra generated files and update sample code

---
+++ sample/src/main/scala/sample/freeMonads/HelloWorld.scala
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2010-2015 HealthExpense, Inc
+ * All Rights Reserved
+ */
+
+package sample.freeMonads
+
+object HelloWorld {
+  def main(args:Array[String]):Unit = {
+    println("Hello, World!")
+  }
+}
